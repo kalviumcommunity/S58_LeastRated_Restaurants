541e642c74a4ebd6fc32148438e18dc65ab9e759
Anushka1205 <anushka.bhatt@kalvium.community>
2024-02-14 16:48:21 +0530

    Updated the port

---
+++ Server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+require("dotenv").config();
+const port = process.env.PORT || 7777;
+
+// define the ping route with the response in JSON
+app.get('/ping', (req, res) => {
+  res.json({ message: 'pong' });
+});
+
+if (require.main === module) {
+  app.listen(port, () => {
+    console.log(`🚀 server running on Port ${port}`);
+  });
+}
+
+module.exports = app;
