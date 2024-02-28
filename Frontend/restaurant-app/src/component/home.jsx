303a32fa2e6a3f0a2e44b5fd2536d54c31e15a2c
Anushka1205 <anushka.bhatt@kalvium.community>
2024-02-28 09:33:05 +0530

    Added frontend folder

---
+++ Frontend/restaurant-app/src/component/home.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import '../App.css'
+import logo from '../assets/logo.png'
+import home_logo from '../assets/home_logo.png'
+import dots_icons from '../assets/dots_icons.png'
+import share from '../assets/share.png'
+
+function Home() {
+
+  return (
+    <>
+        <div className='header'>
+            <img className='logo' src={logo} alt="logo" />
+
+            <input className='search' type="text" placeholder='Search'/>
+
+            <img className='home-logo' src={home_logo} alt="home_logo" />
+
+            <img className='dots-logo' src={dots_icons} alt="dots-icons" />
+
+            <img className='share-btn' src={share} alt="share" />
+
+            <button>Sign Up</button>
+
+        </div>
+        <div className='main'>
+
+        </div>
+    </>
+  )
+}
+
+export default Home
